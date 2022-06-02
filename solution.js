cb19a4cc7e82f252dc639ad65b71cbf4d5af6e50
jang <dkanvk1@gmail.com>
2022-06-02 23:24:24 +0900

    22.6.2

--- solution.js
+++ solution.js
@@ -1,31 +1,10 @@
-function solution(n, arr1, arr2) {
-  const el = n - 1;
-  let count = 0;
-  let temp = "";
-  const tempArr = [];
-  const answer = {};
-  for (let i = 0; i < n; i++) {
-    tempArr.push(arr1[i] | arr2[i]);
+function solution(arr) {
+  const answer = [];
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1]) answer.push(arr[i]);
   }
-  for (let i = 0; i < n; i++) {
-    for (let j = el; j >= 0; j--) {
-      if (tempArr[i] >= 2 ** j) {
-        tempArr[i] -= 2 ** j;
-        temp += "#";
-      } else {
-        temp += " ";
-      }
-    }
-    answer.push(temp);
-    temp = "";
-  }
-  return answer
+  return answer;
 }
-
-// 1. 한 변의 길이 구하기 == n
-// 2. or 연산자 사용
-// 3. 결과 배열 저장
-
-// 3. 결과값을 이진수로 표현 => 1은 #으로 표현
-// for 2 ** n - 1 -> 2 ** 0
-// if 변수값 > element 2거듭제곱 값 => -= 변수값
+// 첫번째 요소는 무조건 푸쉬
+// 두번째 요소부터 앞의 요소와 다를 경우 푸쉬
--- solutionAnswer.js
+++ solutionAnswer.js
@@ -1,23 +1,3 @@
-// 비밀 지도
-function solution(n, arr1, arr2) {
-  let answer = [];
-  for (let i = 0; i < n; i++) {
-    // 입력받은 두 array의 수를 하나씩 가져와서 이진법 변환
-    // 0을 채워서 자릿수 맞추기
-    const arr1Bin = arr1[i].toString(2).padStart(n, 0);
-    const arr2Bin = arr2[i].toString(2).padStart(n, 0);
-    // 만들어진 두 이진수(문자열)을 한글자씩 접근하여 비교하기
-    // 둘 중 하나라도 1이면 #(벽), 둘다 0인경우 공백
-    // 지도의 한 줄
-    let map_row = "";
-    for (let j = 0; j < n; j++) {
-      if (arr1Bin[j] === "1" || arr2Bin[j] === "1") {
-        map_row += "#";
-      } else {
-        map_row += " ";
-      }
-    }
-    answer.push(map_row);
-  }
-  return answer;
+function solution(arr) {
+  return arr.filter((val, index) => val != arr[index + 1]);
 }
