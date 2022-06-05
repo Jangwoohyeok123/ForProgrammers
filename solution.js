0c995e37705b4b91f4f492c56e0ddf9f8a7b6eb0
jang <dkanvk1@gmail.com>
2022-06-05 17:49:38 +0900

    22.6.5

--- solution.js
+++ solution.js
@@ -1,19 +1,5 @@
-function solution(arr, divisor) {
-  let count = 0;
-  const answer = [];
-
-  arr.forEach(element => {
-    if (element % divisor == 0) {
-      count++;
-      answer.push(element);
-    }
-  });
-  let temp = [];
-  temp.push(-1);
-  if (count === 0) return temp;
-  return answer.sort((a, b) => a - b);
+function solution(a, b) {
+  return ((a + b) / 2) * (Math.abs(a - b) + 1);
 }
-// arr 돌면서 각 element를 나눈다.
-// 나눠질 경우 count++, answer.push(element);
-// if count == 0  return -1
-// else return answer.sort((a,b) a-b);
+
+// a + b / 2 * (a-b의 절댓값 + 1) ;
