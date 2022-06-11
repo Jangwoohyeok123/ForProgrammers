d49c295960352c65711d9b2985410f809d8bbda9
jang <dkanvk1@gmail.com>
2022-06-11 19:27:36 +0900

    22.6.11

--- solutionAnswer.js
+++ solutionAnswer.js
@@ -1,22 +1,4 @@
-function solution(s) {
-  let result = false;
-  if ((s.length == 4 || s.length == 6) && /^\d+$/.test(s)) {
-    result = true;
-  }
-  return result;
+function solution(seoul) {
+  const answer = seoul.indexOf("Kim");
+  return `김서방은 ${answer}에 있다`;
 }
-
-/*
-같은코드
-function solution(s){
-  let result = false;
-  if((s.length == 4 || s.length == 6) && /^[0-9]+$/.test(s)) {
-    result = true;
-  }
-  return result;
-}
-^ : 줄의 시작을 정의 위의 코드는 digit으로 시작한다의 의미
-\d : 0123456789 범위
-+ : 문자가 1개 이상 나타남을 의미
-예시 : \^[a-z]+$\.test(s) , \^[0-9a-zA-Z]+$\.test(s) => 정규식에 부합하는지 확인 부합할 경우 true else false 
-*/
