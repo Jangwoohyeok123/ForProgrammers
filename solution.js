76593c2eb96971acabd4caff212f4252c69f5faf
jang <dkanvk1@gmail.com>
2022-06-17 00:14:04 +0900

    22.6.16<-true

--- solution.js
+++ solution.js
@@ -1,19 +1,18 @@
 function solution(s) {
+  let answer = "";
   const arr = s.split(" ");
-  arr.foreach(e => {
-    const temp = Array.from(e);
-    for(let i = 0; i < temp.length ; i++){
-      let tempChar = '';
-      if(i % 2 === 0){
+  for (let i = 0; i < arr.length; i++) {
+    const temp = Array.from(arr[i]);
+    for (let i = 0; i < temp.length; i++) {
+      let tempChar = "";
+      if (i % 2 === 0) {
         tempChar = temp[i].toUpperCase();
       } else {
         tempChar = temp[i].toLowerCase();
       }
-      answer.push(tempChar);
+      answer += tempChar;
     }
-    
-  });
-  const answer = "";
-  console.log(arr);
+    if (i !== arr.length - 1) answer += " ";
+  }
   return answer;
 }
--- solutionAnswer.js
+++ solutionAnswer.js
@@ -1,5 +1,6 @@
-function solution(s) {
-    var answer = '';
-    return answer;
-  }
-  
+function toWeirdCase(s) {
+  //함수를 완성해주세요
+  return s.toUpperCase().replace(/(\w)(\w)/g, function (a) {
+    return a[0].toUpperCase() + a[1].toLowerCase();
+  });
+}
