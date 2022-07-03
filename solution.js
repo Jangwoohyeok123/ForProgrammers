227524f73a36809c36b417398cfa6f1673324b97
jang <dkanvk1@gmail.com>
2022-07-03 21:04:27 +0900

    22.7.3

--- solution.js
+++ solution.js
@@ -1,38 +1,51 @@
-function solution(land) {
-  var answer = 0;
-  let maxIndex = 0;
-  let max = -100000000000;
-  for (let i = 0; i < land.length; i++) {
-    let max = -1;
-    for (let j = 0; j < land[i].length; j++) {
-      if (maxIndex == j) break;
-      if (max < land[i][j]) {
-        max = land[i][j];
-        maxIndex = j;
-      }
+function solution(bridge_length, weight, truck_weights) {
+  let sec = 1;
+  let curWeight = 0;
+  const queBridge = Array.from({ length: bridge_length }, () => 0);
+
+  const init = truck_weights.shift();
+  queBridge.push(init);
+  queBridge.shift();
+  curWeight += init;
+
+  while (curWeight !== 0) {
+    const firstCar = truck_weights.shift();
+    curWeight -= queBridge.shift();
+    if (curWeight + firstCar <= weight) {
+      curWeight += firstCar;
+      queBridge.push(firstCar);
+    } else {
+      queBridge.push(0);
+      truck_weights.unshift(firstCar);
     }
-    answer += tmp;
+    sec++;
   }
-  return answer;
+  return sec;
 }
 
-solution([
-  [23, 37, 13, 2]
-]);
+/*
+1 7
+2 7
+3 4
+4 4,5
+5 5
+6 6
+7 6
+8 
+
+0 ~ 1 = 1
+*/
 
 /*
-=> 다 도는 방식으로 하면 시간초과 불보듯 뻔함
-=> 각 단계에서 최댓값을 찾으면서 가야한다.
-=> land.length : 행의 개수
-=> lang[~~].length : 열의 개수
+다리의 구조 : 일정 크기를 유지해야 하며 1초마다 무조건 shift가 되어야 하고, 1초마다 차가 들어오지 않아도 qush를 해줘야 한다. ? 크기를 유지시켜야함
 */
 
-/* version1
-1. 행의 수 만큼 도는 배열만들기
-2. 전 행의 최댓값을 기억하는 변수만들기
-3. 변수를 제외한 행의 열 중 가장 큰 값을 더하기.. 변수와 일치할 경우 continue
+/* 
+  1초마다 넣을 것인가? => 다리 밖 줄세우기
+  => if weight 이하일 경우 && bridge_length보다 작을 경우 넣는다. => 넣을 경우 curWeight += el; 
+  => else 넣지 않는다.
+  => [0]에 위치할 경우 shift를 한다. => 이 경우 curWeight -= el;
 */
-
-/* version2
-
+/* outer loop 
+1. 다리의 무게가 0인 경우에 멈춘다.
 */
--- solutionAnswer.js
+++ solutionAnswer.js
@@ -1,40 +1,31 @@
-function solution(land) {
-  for(let i=1; i<land.length; i++) {
-      land[i][0] += Math.max(
-          land[i-1][1],
-          land[i-1][2],
-          land[i-1][3],
-      );
-      land[i][1] += Math.max(
-          land[i-1][0],
-          land[i-1][2],
-          land[i-1][3],
-      );
-      land[i][2] += Math.max(
-          land[i-1][0],
-          land[i-1][1],
-          land[i-1][3],
-      );
-      land[i][3] += Math.max(
-          land[i-1][0],
-          land[i-1][1],
-          land[i-1][2],
-      );
+function solution(bridge_length, weight, truck_weights) {
+  let time = 0;
+  let bridge = [];
+  let bridge_weight = 0;
+
+  for (let i = 0; i < bridge_length; i++) {
+    bridge.push(0);
   }
-  return Math.max(...land[land.length-1]);
+
+  const truck = truck_weights.shift();
+
+  bridge.push(truck);
+  bridge.shift();
+  bridge_weight += truck;
+  time++;
+
+  while (bridge_weight !== 0) {
+    const next_truck = truck_weights.shift();
+    bridge_weight -= bridge.shift();
+    if (next_truck + bridge_weight <= weight) {
+      bridge.push(next_truck);
+      bridge_weight += next_truck;
+    } else {
+      bridge.push(0);
+      truck_weights.unshift(next_truck);
+    }
+    time++;
+  }
+  //경과한 시간
+  return time;
 }
-
-/*
-=> 헷갈리면 안되는 것
-=> 재귀 호출시 return 안에 자기자신이 들어가지 않아도 위와 같이 작성하면 됨
-=> 깊이 우선 탐색의 경우 하나하나 끝까지 다 가보는 방법으로 보통 재귀로 품
-=> 왜 왼쪽 오른쪽 둘다 가고 그 의미는?
-=> 그 전에 그냥 1차원 배열인데 구글에서의 그래프랑 뭔 연계가?
-=> 인덱스로 생각
-=> 1: 2,3,4,5  2: 1,3,4,5 1차원 배열을 그 뭐야 경우의 수 세듯이 하면 그래프
-=> 구조를 떠올릴 수 있음 
-*/
-
-/* 1차원 배열의 경우의 수 찾기
-=> 
-*/
