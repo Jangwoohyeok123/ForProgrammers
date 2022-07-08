f76258a0fb809756c1286006f8102f884a6644a7
jang <dkanvk1@gmail.com>
2022-07-08 21:58:42 +0900

    22.7.8

--- solution.js
+++ solution.js
@@ -1,10 +1,16 @@
-function solution(x, n) {
-  const answer = [];
-  const commonRatio = x;
-  let tmp = 0;
-  for(let i = 0; i < n; i++){
-      tmp += commonRatio;
-      answer.push(tmp);
+const combinations = function(menu, order, cnt, idx, prev){
+  if(prev.length === cnt) {
+      
   }
+}
+function solution(orders, course) {
+  let answer = [];
+  let prev_elements = [];
+  function combination(elements, k, results) {
+      if(prev_elements.length === k){
+          let str_tmp = prev_elements.
+      }
+  }
+  var answer = [];
   return answer;
 }
--- solutionAnswer.js
+++ solutionAnswer.js
@@ -1,1 +1,29 @@
-
+function solution(orders, course){
+  let answer = [];
+  course.forEach(num => {
+    const menus = new Map();
+    orders.forEach((order) => {
+      const combinations = getCombination(order.split('').sort(), num);
+      combinations.forEach(combs => {
+        const comb = combs.join('');
+        menus.set(comb, menus.has(comb) ? menus.get(comb) + 1 : 1);
+      });
+    });
+  })
+  let populate = Math.max(...menus.vvalues());
+  menus.forEach((count, menu, map) => {
+    if(count === populate && count >= 2) answer.push(menu);
+  });
+  return answer.sort();
+}
+const result = [];
+function getCombination(arr, selectNum){
+  if(selectNum == 1) return arr.map(v => [v]);
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1);
+    const combination = getCombination(rest, selectNum - 1);
+    const attached = combination.map(v => [fixed, ...v]);
+    result.push(...attached);
+  });
+  return result;
+}
