649e133cf56c76bdad304d07baffa6b7374fefdb
jang <dkanvk1@gmail.com>
2022-07-14 23:57:34 +0900

    22.7.14

--- solution.js
+++ solution.js
@@ -1,37 +1,15 @@
-function solution(numbers) {
-  var answer = [];
-  let nums = numbers.split("");
-  // 소수 판별
-  const isPrimeNum = num => {
-    if (num <= 1) return false;
-    for (let i = 2; i * i <= num; i++) {
-      if (num % i === 0) return false;
+function solution(n) {
+  let i = 1;
+  var answer = 1;
+  let sum = 0;
+  while (n / i > 2) {
+    for (let j = i; j < n; j++) {
+      sum += j;
+      if (sum > n) break;
+      if (sum == n) answer++;
     }
-    return true;
-  };
-  // 순열 만들기
-  const getPermutation = (arr, fixed) => {
-    if (arr.length >= 1) {
-      for (let i = 0; i < arr.length; i++) {
-        const newNum = fixed + arr[i];
-        const copyArr = [...arr];
-        copyArr.splice(i, 1);
-        if (!answer.includes(+newNum) && isPrimeNum(+newNum)) {
-          answer.push(+newNum);
-        }
-        console.log(+newNum);
-        console.log(newNum);
-        getPermutation(copyArr, newNum);
-      }
-    }
-  };
-  getPermutation(nums, "");
-  return answer.length;
+    i++;
+    sum = 0;
+  }
+  return answer;
 }
-
-// 하나는 내림차순
-// 하나는 오름차순
-// sort 는 원배열이 정렬
-// compareFunction이 제공되지 않으면 요소를 문자열로 변환하고 유니 코드 코드 포인트 순서로 문자열을 비교하여 정렬됩니다. 예를 들어 "바나나"는 "체리"앞에옵니다. 숫자 정렬에서는 9가 80보다 앞에 오지만 숫자는 문자열로 변환되기 때문에 "80"은 유니 코드 순서에서 "9"앞에옵니다.
-// 함수값이 -면 a 가 앞으로
-// 함수값이 +면 b 가 앞으로
--- solutionAnswer.js
+++ solutionAnswer.js
@@ -1,28 +1,8 @@
-function solution(numbers) {
-  var answer = [];
-  let nums = numbers.split("");
-  // 소수 판별
-  const isPrimeNum = num => {
-    if (num <= 1) return false;
-    for (let i = 2; i * i <= num; i++) {
-      if (num % i === 0) return false;
-    }
-    return true;
-  };
-  // 순열 만들기
-  const getPermutation = (arr, fixed) => {
-    if (arr.length >= 1) {
-      for (let i = 0; i < arr.length; i++) {
-        const newNum = fixed + arr[i];
-        const copyArr = [...arr];
-        copyArr.splice(i, 1);
-        if (!answer.includes(+newNum) && isPrimeNum(+newNum)) {
-          answer.push(+newNum);
-        }
-        getPermutation(copyArr, newNum);
-      }
-    }
-  };
-  getPermutation(nums, "");
-  return answer.length;
+function solution(n) {
+  let answer = 0;
+
+  for (let i = 0; i <= n; i++) {
+    if (n % i === 0 && i % 2 === 1) answer++;
+  }
+  return answer;
 }
